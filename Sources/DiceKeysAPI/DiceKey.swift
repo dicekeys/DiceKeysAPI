79330ed8d0624126353f94e86ea5f90944877cff
Stuart Schechter <stuart@dicekeys.com>
2020-11-13 09:42:54 +0900

    continue porting

--- Sources/DiceKeysAPI/DiceKey.swift
+++ Sources/DiceKeysAPI/DiceKey.swift
@@ -7,11 +7,93 @@
 
 import Foundation
 
-typealias DiceKey = (
-    Face, Face, Face, Face, Face,
-    Face, Face, Face, Face, Face,
-    Face, Face, Face, Face, Face,
-    Face, Face, Face, Face, Face,
-    Face, Face, Face, Face, Face
+typealias Tuple25<T> = (
+    T, T, T, T, T,
+    T, T, T, T, T,
+    T, T, T, T, T,
+    T, T, T, T, T,
+    T, T, T, T, T
 )
 
+typealias FaceTuple = Tuple25<Face>
+
+let clockwise90DegreeRotationIndexesFor5x5Grid = [
+    20, 15, 10, 5, 0,
+    21, 16, 11, 6, 1,
+    22, 17, 12, 7, 2,
+    23, 18, 13, 8, 3,
+    24, 19, 14, 9, 4
+]
+
+class DiceKey {
+    let faces: [Face]
+    
+    let faceTuple: FaceTuple { get {
+        return (
+            faces[0], faces[1], faces[2], faces[3], faces[4],
+            faces[5], faces[6], faces[7], faces[8], faces[9],
+            faces[10], faces[11], faces[12], faces[13], faces[14],
+            faces[15], faces[16], faces[17], faces[18], faces[19],
+            faces[20], faces[21], faces[22], faces[23], faces[24]
+        )
+    }}
+
+    init(faces: [Face]) {
+        precondition(faces.count == 25)
+        self.faces = faces;
+    }
+    
+    func withoutOrientations() -> DiceKey {
+        return DiceKey(
+            faces.map() {
+                Face(
+                    letter: $0.letter,
+                    digit: $0.digit,
+                    orientationAsLowercaseLetterTrbl: FaceOrientationLetterTrbl.Top
+                )
+            }
+        )
+    }
+        
+    func rotatedClockwise90Degrees() -> DiceKey {
+        return DiceKey(
+            clockwise90DegreeRotationIndexesFor5x5Grid.map() { index in
+                Face(
+                    letter: faces[index].letter,
+                    digit: faces[index].digit,
+                    orientationAsLowercaseLetterTrbl: faces[index].orientationAsLowercaseLetterTrbl.rotate90() )
+
+            }
+        )
+    }
+    
+    func toHumanReadableForm(includeOrientations: Bool) -> String {
+        return faces.map() { face -> String in
+            face.letter.rawValue +
+            face.digit +
+            (includeOrientations ? face.orientationAsLowercaseLetterTrbl : "")
+        }.joined(separator: "") as String
+    }
+    
+    func rotatedToCanonicalForm(
+      includeOrientations: boolean
+    ) -> DiceKey {
+        var candidateDiceKey = self
+        var diceKeyWithEarliestHumanReadableForm = currentDiceKey
+        var earliestHumanReadableForm = diceKeyWithEarliestHumanReadableForm.toHumanReadableForm(includeOrientations)
+        for rotation in 1...3 {
+            candidateDiceKey = candidateDiceKey.rotatedClockwise90Degrees()
+            let humanReadableForm = candidateDiceKey.toHumanReadableForm(includeOrientations)
+            if (humanReadableForm < earliestHumanReadableForm) {
+                earliestHumanReadableForm = humanReadableForm
+                diceKeyWithEarliestHumanReadableForm = currentDiceKey
+            }
+        }
+        return rotationIndependentDiceKey
+    }
+    
+    func toSeed(includeOrientations: boolean) {
+        return rotatedToCanonicalForm(includeOrientations).toHumanReadableForm(includeOrientations)
+    }
+    
+}
--- Sources/DiceKeysAPI/Face.swift
+++ Sources/DiceKeysAPI/Face.swift
@@ -7,28 +7,26 @@
 
 import Foundation
 
-//enum FaceLetter: String, Codable {
-//    case A, B, C, D, F, G, H, I, J, K, L, M, N, O, P, R, S, T, U, V, W, X, Y, Z
-//}
-//
-//enum FaceDigit: String, Codable {
-//    case D1 = "1"
-//    case D2 = "2"
-//    case D3 = "3"
-//    case D4 = "4"
-//    case D5 = "5"
-//    case D6 = "6"
-//}
-//
-//enum FaceOrientationLetterTrbl: String, Codable {
-//    case Top = "t"
-//    case Right = "r"
-//    case Bottom = "b"
-//    case Left = "l"
-//}
+extension FaceOrientationLetterTrbl {
+    func rotate90() -> FaceOrientationLetterTrbl {
+        switch self {
+            case FaceOrientationLetterTrbl.Top: return FaceOrientationLetterTrbl.Right
+            case FaceOrientationLetterTrbl.Right: return FaceOrientationLetterTrbl.Bottom
+            case FaceOrientationLetterTrbl.Bottom: return FaceOrientationLetterTrbl.Left
+            case FaceOrientationLetterTrbl.Left: return FaceOrientationLetterTrbl.Top
+        }
+    }
+}
 
 struct Face {
     let letter: FaceLetter
     let digit: FaceDigit
     let orientationAsLowercaseLetterTrbl: FaceOrientationLetterTrbl
+    
+    func rotate90() -> Face {
+        return Face(letter: letter, digit: digit, orientationAsLowercaseLetterTrbl: self.orientationAsLowercaseLetterTrbl.rotate90()
+        )
+    }
 }
+
+
